542cdcdf5694111efc11c7de848eabb8a9843d9b
Margarita Omelchenko <margotgreed@gmail.com>
2025-10-26 15:55:12 +0300

    *

--- Domoplat.System.MessageQueue/Config/KafkaConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Domoplat.System.MessageQueue.Config;
-
-public class KafkaConfig
-{
-    public string BootstrapServers { get; set; }
-    
-    public string GroupId { get; set; }
-}
--- Domoplat.System.MessageQueue/Consuming/IMessageConsumeHandler.cs
+++
@@ -1,6 +0,0 @@
-namespace Domoplat.System.MessageQueue.Consuming;
-
-public interface IMessageConsumeHandler<TMessage>
-{
-    Task Handle(TMessage message, CancellationToken cancellationToken);
-}
